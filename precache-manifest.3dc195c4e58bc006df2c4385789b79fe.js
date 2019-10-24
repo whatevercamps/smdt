55ede46e4fa287d036da67e3cdd6bebdb0806510
David Bautista <dj.bautista10@uniandes.edu.co>
2019-10-23 20:31:45 -0500

    primer commit

---
+++ precache-manifest.3dc195c4e58bc006df2c4385789b79fe.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57bb13df822a5657e4a869928030abc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe8a6a3f873f123afe0e",
+    "url": "/static/css/2.3cecf958.chunk.css"
+  },
+  {
+    "revision": "5d15c8bdbc0c771613d0",
+    "url": "/static/css/main.1b2d1f43.chunk.css"
+  },
+  {
+    "revision": "fe8a6a3f873f123afe0e",
+    "url": "/static/js/2.e31998aa.chunk.js"
+  },
+  {
+    "revision": "5d15c8bdbc0c771613d0",
+    "url": "/static/js/main.fb6a3953.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "923455be1f667836547cad3dc57148eb",
+    "url": "/static/media/Twitter_Bird.923455be.svg"
+  },
+  {
+    "revision": "ce7a6b79aa55041f7ae36f6ce22231fe",
+    "url": "/static/media/face-3.ce7a6b79.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "34535b958ad0f0183770a8731b31b4ca",
+    "url": "/static/media/sidebar-3.34535b95.jpg"
+  }
+]);
